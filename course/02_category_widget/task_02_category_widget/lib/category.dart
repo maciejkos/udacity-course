0a6410ca23f5c3b525db8a5cb6653b12476e336c
mkos <mk@mk.pl>
2019-04-23 11:53:19 -0400

    task 2 and 3 done

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -10,24 +10,77 @@ import 'package:flutter/material.dart';
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
+///
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 class Category extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
 
   /// Builds a custom widget that shows [Category] information.
   ///
   /// This information includes the icon, name, and color for the [Category].
   @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
+// This `context` parameter describes the location of this widget in the
+// widget tree. It can be used for obtaining Theme data from the nearest
+// Theme ancestor in the tree. Below, we obtain the display1 text theme.
+// See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Material(
+      color: Colors.transparent,
+      borderRadius: _borderRadius,
+      child: Container(
+        height: _rowHeight,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            onTap: () {
+              print('I was tapped!');
+            },
+            borderRadius: _borderRadius,
+            highlightColor: color,
+            splashColor: color,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(iconLocation, size: 60)),
+                Center(
+                  child: Text(
+                    'This is a cake!',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+//      height: 100.0,
+//      padding: 8.0,
+//      radius: this.height/2
+      ),
+    );
   }
 }
