9fe51b13b963a5cb6c1de9523f22503276d2df72
mkos <mk@mk.pl>
2019-04-30 19:00:20 -0400

    task 6 done

---
+++ course/06_input/task_06_input/lib/playground.dart
@@ -0,0 +1,53 @@
+// This sample shows a `DropdownButton` whose value is one of
+// "One", "Two", "Free", or "Four".
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Code Sample for material.DropdownButton',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+String dropdownValue = 'One';
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          onChanged: (String newValue) {
+            setState(() {
+              dropdownValue = newValue;
+            });
+          },
+          items: <String>['One', 'Two', 'Free', 'Four']
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
